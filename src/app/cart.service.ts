b4ff5e5a92ee4eabdf5171abf0fa1c7b84e3bdac
naz-k <f.kashandeh95@yahoo.com>
2021-09-27 10:46:25 -0400

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -5,4 +5,17 @@ import { Product } from './products';
 export class CartService {
   items: Product[] = [];
   constructor() {}
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
